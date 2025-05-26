e5eb4892c9b20efce55a9e51a2893a2d79600fff
Moiz Ansari <moizansari.india@gmail.com>
2025-05-27 00:11:44 +0530

    creating login and signup page

--- app/components/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <>
-
-            <div>Coming from the windows to make the points</div>
-            <div>Next.js has file-based rendering</div>
-        </>
-    )
-}
