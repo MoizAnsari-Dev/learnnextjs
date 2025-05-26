eb8cd91d278e9d86285a0625615332185968f180
Moiz Ansari <moizansari.india@gmail.com>
2025-05-26 23:41:06 +0530

    It has file based randering

---
+++ app/components/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+
+            <div>Coming from the windows to make the points</div>
+            <div>Next.js has file-based rendering</div>
+        </>
+    )
+}
