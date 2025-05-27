fa99dffcc7ebe6336529ad906189a5a3dea0e9d7
Moiz Ansari <moizansari.india@gmail.com>
2025-05-27 22:13:34 +0530

    Fixing some erroe

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(true);
  
-  useEffect(() => {
-    axios
-  })
   return (
     <>
       <div className="h-screen w-screen flex flex-col items-center justify-center">
-       
-        </button>
+        <h1 className="text-5xl">Wellcome to the Home Page</h1>
       </div>
     </>
   );
